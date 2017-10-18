34db315cca9d048cb6031d2289ebb6b7c4214f6b
Thomas D Williams <dwilliams@halehan.com>
2017-10-17 19:34:31 -0500

    Adding

--- entities/quote.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const mongoose = require("mongoose");
-;
-var quoteSchema = new mongoose.Schema({
-    text: String,
-    author: String
-});
-var Quote = mongoose.model("Quote", quoteSchema);
-module.exports = Quote;
