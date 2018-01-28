bdfad8d1fdbcd00c26fa1385ac0da4b2f0166033
Thomas D Williams <dwilliams@halehan.com>
2018-01-27 21:43:53 -0600

    Added support for location

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export class Constants {
+   // static readonly DATE_FMT = 'dd/MMM/yyyy';
+    static readonly DATE_FMT = 'MMM-dd-yyyy';
+    static readonly DATE_TIME_FMT = `${Constants.DATE_FMT} hh:mm:ss a`;
+
+    static readonly SERVER_URL = 'https://wcso-api-ts.herokuapp.com/';
+
+    static readonly FACEBOOK_VERIFY_TOKEN = 'halehan';
+    static readonly FACEBOOK_PAGE_VERIFY_TOKEN = 'EAAHuAlckN1IBAIZBiZB9dfqXpEi9zk0PyzOd7sG7RwHALntFtxxEEFSt8o1CJcrMrW1bGMYkD4LQN0s1LZCDqknBziTvImDLBAsqIYiFEtaEOaALEoNfnFoI0DY986tpsjBPlDQsZAzhXJhTjtIY9IP9A6rwBujHz4jsH7vZBt4NN61BuUZCZBIKtdWV3nFihMZD';
+    
+    static readonly GOOGLE_API_KEY = 'AIzaSyAGZ6xu-PUDubQLdxQvBl5DeJgXscWEbPo';
+  }
