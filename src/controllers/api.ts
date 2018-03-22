b7f6036d73c587dc0c66ca966a48ce99caff83b6
Thomas D Williams <dwilliams@halehan.com>
2018-03-22 10:46:36 -0500

    adedd activity entity

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -304,6 +304,7 @@ export let authenticate = (req: Request, res: Response) => {
         res.json({ success: false, message: 'Authentication failed. Wrong password.' });
         this.putActivity(this.loginId, 'Authentication failed. Wrong password.');
       } else {
+        this.putActivity(this.loginId, 'Success');
 
       // if user is found and password is right
       // create a token with only our given payload
@@ -322,7 +323,7 @@ export let authenticate = (req: Request, res: Response) => {
           token: token
         });
 
-        this.putActivity(this.loginId, 'Success');
+       
       }   
 
     }
